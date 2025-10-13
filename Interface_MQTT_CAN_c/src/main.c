0466c2a6184aff186d7827370829c7a87289e2a6
Joseph NKOULOU <joseph.nkoulou@2026.icam.fr>
2025-10-13 09:47:27 +0200

    Compilation Code Refactorisé ok

--- Interface_MQTT_CAN_c/src/main.c
+++ Interface_MQTT_CAN_c/src/main.c
@@ -1,8 +1,8 @@
 #include "bridge_app.h"
 #include <stddef.h>
 
-int main(){
-  if(my_setup) return 1;
+int main(void){
+  if (!my_setup()) return 1;
   while(my_loop()) { /* loop */ }
   my_shutdown();
   return 0;
