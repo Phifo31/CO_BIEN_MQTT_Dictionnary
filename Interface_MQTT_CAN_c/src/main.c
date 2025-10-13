446f4d974497110d0db79a53d5837abd537e22c5
Joseph NKOULOU <joseph.nkoulou@2026.icam.fr>
2025-10-13 12:04:01 +0200

    Tests

--- Interface_MQTT_CAN_c/src/main.c
+++ Interface_MQTT_CAN_c/src/main.c
@@ -1,9 +1,13 @@
 #include "bridge_app.h"
 #include <stddef.h>
 
-int main(void){
-  if (!my_setup()) return 1;
-  while(my_loop()) { /* loop */ }
+int main(int argc, char **argv){
+  const char *cfg = (argc > 1) ? argv[1] : NULL;
+  if (!my_setup(cfg)) return 1;
+  while (my_loop()) {}
   my_shutdown();
   return 0;
 }
+
+
+
